5b2dd2284c25b3a13c2853ead81d437a75d4e59d
naohr <najmim625@gmail.com>
2022-12-24 14:17:58 +0700

    update: configure mantine

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { createGetInitialProps } from '@mantine/next';
 
 export default function Document() {
+  const getInitialProps = createGetInitialProps();
+
   return (
     <Html lang="en">
       <Head />

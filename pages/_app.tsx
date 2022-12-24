c71f298343c0ff78dc30e1d002dff0746c7466aa
naohr <najmim625@gmail.com>
2022-12-24 15:40:55 +0700

    refactor: implement global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,15 @@ export default function App({ Component, pageProps }: AppProps) {
       withGlobalStyles
       withNormalizeCSS
       theme={{
-        colorScheme: 'dark'
+        colorScheme: 'dark',
+        globalStyles: (theme) => ({
+          body: {
+            ...theme.fn.fontStyles(),
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[2],
+            color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.dark[4],
+            lineHeight: theme.lineHeight,
+            }
+        })
       }}
     >
       <Component {...pageProps} />

98bcea11025df90a1a720fbc397f53c75b3c7139
naohr <najmim625@gmail.com>
2022-12-25 12:01:38 +0700

    add: getAllData and implement authentication middleware

---
+++ pages/api/get/allData.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import decideMethod from "../../../backend/middleware/decidemethodMiddleware";
+import authenticationMiddleware from "../../../backend/middleware/authenticationMiddleware";
+
+import connectDB from "../../../backend/middleware/connectDB";
+import errorHandler from "../../../backend/utils/errorHandler";
+import CustomerModel from "../../../backend/models/Customer.model";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    try{
+
+        const custs = await CustomerModel.find();
+        return res.status(200).json({
+            ok: true,
+            message: "success",
+            data: custs
+        })
+
+    }catch(e){
+        console.log(e)
+        const newE = e as Error;
+        return errorHandler(newE, res);
+    }
+}
+
+export default authenticationMiddleware(decideMethod(connectDB(handler), "get"))

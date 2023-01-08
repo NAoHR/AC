ebc210dc1f346e5b476052b65684003c834ded62
naohr <najmim625@gmail.com>
2023-01-08 11:18:36 +0700

    update: add logout button

--- components/HeaderComp.tsx
+++ components/HeaderComp.tsx
@@ -1,4 +1,5 @@
 import {Dispatch, SetStateAction} from "react";
+import { useRouter } from "next/router";
 
 import {
     useMantineTheme,
@@ -6,9 +7,12 @@ import {
     Text,
     MediaQuery,
     Burger,
-    Avatar
+    Avatar,
+    Flex,
+    Button
 } from '@mantine/core';
-import { IconAirConditioning } from '@tabler/icons';
+import { IconAirConditioning, IconLogout } from '@tabler/icons';
+import {toast} from "react-toastify"
 
 
 interface HeaderInterface {
@@ -20,24 +24,36 @@ export default function HeaderComp(props: HeaderInterface){
     const { opened, setOpened } = props;
     const theme = useMantineTheme();
 
+    const router = useRouter();
     return (
         <>
         <Header height={{ base: 70 }} p="md">
-          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o : boolean) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-            <Avatar color="blue" radius="sm" mr={"sm"}>
-              <IconAirConditioning size={20} />
-            </Avatar>
-            <Text fw={"700"}>AC Management System</Text>
-          </div>
+          <Flex align={"center"} justify="space-between">
+            <Flex align={"center"} h="100%" justify={"flex-start"}>
+              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o : boolean) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+              <Avatar color="blue" radius="sm" mr={"sm"}>
+                <IconAirConditioning size={20} />
+              </Avatar>
+              <MediaQuery smallerThan={"sm"} styles={{display: "none"}}>
+                <Text fw={"700"}>AC Management System</Text>
+              </MediaQuery>
+            </Flex>
+            <Button variant="outline" radius={"sm"} onClick={() => {
+              window.localStorage.clear();
+              toast.success("Kamu telah logout")
+              router.push("/login")
+            }}>
+              <IconLogout size={18} />
+            </Button>
+          </Flex>
         </Header>
         </>
     )
